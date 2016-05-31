a2ae0f4d6acbae8718f8d581193b4185acaaed7c
MarekAG <marekwstrzala@gmail.com>
2016-05-31 20:56:44 +0200

    added simple movie displaying

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+export class Movie {
+    id: number;
+    name: string;
+}
 @Component({
     selector: 'my-app',
-    template: '<h1>Movie Library</h1>'
+    template:`
+    <h1>{{title}}</h1>
+    <h2>{{movie.name}} details!</h2>
+    <div><label>id: </label>{{movie.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="movie.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Movie Library';
+    movie: Movie = {
+        id: 1,
+        name: 'Titanic'
+    };
+}
